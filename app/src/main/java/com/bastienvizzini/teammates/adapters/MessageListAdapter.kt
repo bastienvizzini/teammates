7c53a6c8f19fee85589e6f3e6d3b6b577478b58d
bastienvizzini <bastienvizzini@gmail.com>
2021-02-24 18:27:37 +0100

    Message adapter created and implemented

--- app/src/main/java/com/bastienvizzini/teammates/adapters/MessageListAdapter.kt
+++ app/src/main/java/com/bastienvizzini/teammates/adapters/MessageListAdapter.kt
@@ -1,4 +1,44 @@
 package com.bastienvizzini.teammates.adapters
 
-class MessageListAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bastienvizzini.teammates.R
+import com.bastienvizzini.teammates.models.Message
+
+class MessageListAdapter(context: Context, messageList: MutableList<Message>): RecyclerView.Adapter<MessageListAdapter.MessagesViewHolder>() {
+
+    private var mMessageList = messageList
+    private var mInflater = LayoutInflater.from(context)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessagesViewHolder {
+        val itemView = mInflater.inflate(R.layout.item_message_list, parent, false)
+        return MessagesViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return mMessageList.size
+    }
+
+    override fun onBindViewHolder(holder: MessagesViewHolder, position: Int) {
+        val currentMessage = mMessageList[position]
+
+        // To be modified
+        holder.usernameTextView.text = currentMessage.userSenderUid
+        holder.messageTextView.text = currentMessage.message
+        // To be modified
+        holder.pictureImageView.setImageResource(R.drawable.ic_person)
+    }
+
+
+    inner class MessagesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val usernameTextView: TextView = itemView.findViewById(R.id.usernameMessageListItem)
+        val messageTextView: TextView = itemView.findViewById(R.id.messageMessageListItem)
+        val pictureImageView: ImageView = itemView.findViewById(R.id.pictureMessageListItem)
+    }
+
 }
--- app/src/main/java/com/bastienvizzini/teammates/fragments/MessagesFragment.kt
+++ app/src/main/java/com/bastienvizzini/teammates/fragments/MessagesFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bastienvizzini.teammates.R
+import com.bastienvizzini.teammates.adapters.MessageListAdapter
+import com.bastienvizzini.teammates.models.Message
+import kotlinx.android.synthetic.main.fragment_messages.*
 
 class MessagesFragment : Fragment() {
 
@@ -15,6 +19,22 @@ class MessagesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_messages, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mMessageList: MutableList<Message> = mutableListOf()
+
+        for (i in 0..9) {
+            val message = Message("Hey man, I'm message ${i+1}", userSenderUid = "User ${i+1}")
+            mMessageList.add(message)
+        }
+
+        val mAdapter = MessageListAdapter(requireContext(), mMessageList)
+        messagesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        messagesRecyclerView.adapter = mAdapter
+
     }
 
 }
